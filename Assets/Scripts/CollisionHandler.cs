61ad19e7781efad4a9015af12139826d58204395
W28\wojte <wojtex28@gmail.com>
2023-07-03 23:46:30 +0200

    - Added tags for checking collision - Used Switch case to verify type of object im collided with - Added Scene into build settings - Created CollisionHandler which resets the scene after contact with wrong collider

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("You have bumped into friendly!");
+                break;
+            case "Finish":
+                Debug.Log("Congratulations! You have finished the level!");
+                break;
+            case "Fuel":
+                Debug.Log("You have picked up fuel!");
+                break;
+            default:
+                ReloadLevel();
+                break;
+        }
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
