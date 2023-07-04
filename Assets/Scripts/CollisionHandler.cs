39159c3e5e9dd1041b692eb2d733bbd15916df26
WojciechW <wojciech.wlodyka@takeitstudio.com>
2023-07-04 18:01:52 +0200

    Switch update, scene changer
    - Added abillity to change scenes, additional scene

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,32 +1,66 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Accessibility;
+using System;
 
 public class CollisionHandler : MonoBehaviour
 {
+
+    [SerializeField] public float loadLevelDelay = 1.0f;
     void OnCollisionEnter(Collision other)
     {
         switch (other.gameObject.tag)
         {
             case "Friendly":
-                Debug.Log("You have bumped into friendly!");
+                StartSucessSequence();
                 break;
             case "Finish":
-                Debug.Log("Congratulations! You have finished the level!");
+                Invoke("LoadNextLevel",loadLevelDelay);
                 break;
             case "Fuel":
                 Debug.Log("You have picked up fuel!");
                 break;
             default:
-                ReloadLevel();
+                StartCrashSequence();
                 break;
         }
+    }
+
+    private void StartSucessSequence()
+    {
+        throw new NotImplementedException();
+    }
+
+    void StartCrashSequence()
+    {
+        StopPlayerMovement();
+        Invoke("ReloadLevel", loadLevelDelay);
     }
 
     void ReloadLevel()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextLevel()
+    {
+        StopPlayerMovement();
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int loadNextLevel = currentSceneIndex + 1;
+        if (loadNextLevel == SceneManager.sceneCountInBuildSettings)
+        {
+           loadNextLevel = 0;
+        }
+        SceneManager.LoadScene(loadNextLevel);
+
+    }
+
+    void StopPlayerMovement()
+    {
+        GetComponent<Movement>().enabled = false;
     }
 }
